056814a9ce1f09e5792297df5ea966f0d911f840
Fufu Fang <fangfufu@fangfufu.co.uk>
2026-09-24 00:29:00 +0100

    added the initial website

---
+++ public_html/hamburger.js
@@ -0,0 +1,12 @@
+/*Toggles menu on/off when user clicks icon*/
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
